3e4f80ba521d64ce0d3a9f8204dd0bebd45ae0b6
mrosati84 <rosati.matteo@gmail.com>
2023-08-28 20:25:16 +0200

    add dummy pages

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+    </div>
+  );
+}
+
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
-function Home() {
-  return <div>Hello</div>;
+import Link from 'next/link';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <p>
+        <Link href="/about">About</Link>
+      </p>
+    </div>
+  );
 }
 
-export default Home;
+export default HomePage;
