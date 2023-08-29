f853cbb64353535be1dcd928c999762f7bd0f24a
Matteo Rosati <matteo.rosati@akqa.com>
2023-08-29 15:13:36 +0200

    setup basic routes

---
+++ pages/events/[...slug].js
@@ -0,0 +1,9 @@
+function EventsFilterPage() {
+  return (
+    <div>
+      <h1>Events Filtered Page</h1>
+    </div>
+  );
+}
+
+export default EventsFilterPage;
---
+++ pages/events/[eventid].js
@@ -0,0 +1,5 @@
+function EventPage() {
+  return <h1>The Event Page</h1>;
+}
+
+export default EventPage;
